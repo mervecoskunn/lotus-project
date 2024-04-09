c2fdaffc8a41cb3fe746ad32a9b3724608d5b5e2
Merve Coskun <merve_coskun2013@hotmail.com>
2024-04-09 15:30:07 +0300

    login and register view functions

--- lotus/users/views.py
+++ lotus/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'users/login.html')
+
+
+def register(request):
+    return render(request, 'users/register.html')
