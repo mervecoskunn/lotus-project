630f2f866594f151f429f68dc4bab9b1b96c2406
Merve Coskun <merve_coskun2013@hotmail.com>
2024-04-09 15:30:41 +0300

    login and register urls added to lotus/urls.py

--- lotus/lotus/urls.py
+++ lotus/lotus/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', users_views.login, name="login"),
+    path('register/', users_views.register, name="register"),
 ]
