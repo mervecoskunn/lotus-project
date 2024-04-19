10d2565b08c5042b21a4c0d22bf8215ac77adb59
Merve Coskun <merve_coskun2013@hotmail.com>
2024-04-19 22:13:00 +0300

    products.html

--- lotus/shopping/views.py
+++ lotus/shopping/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 
 
 def shopping(request):
-    return render(request, 'shopping/shopping.html')
+    context = {
+        "products": models.product_list
+    }
+    return render(request, 'shopping/shopping.html', context)
