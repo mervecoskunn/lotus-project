3aac909988d6e87d0d5ca951a20b71949b85e672
Merve Coskun <merve_coskun2013@hotmail.com>
2024-05-02 21:31:40 +0300

    secret key secured

--- lotus/lotus/settings.py
+++ lotus/lotus/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*5uh32uvt!sq509k&c-oim-n=&987_5(dszc5+59d!-=n6s^38'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
