154c563ce9d2e63db8ccfc655f16b2b6a1e41f96
Merve Coskun <merve_coskun2013@hotmail.com>
2024-05-10 11:00:25 +0300

    Admin - Post CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,9 @@ from . import views as blog_views
 urlpatterns = [
     path('', blog_views.post_list, name='post_list'),
     path('post/<int:pk>/', blog_views.post_detail, name='post_detail'),
+    path('post/new/', blog_views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', blog_views.post_edit, name='post_edit'),
+    path('post/<int:pk>/post_confirm_delete/', blog_views.post_confirm_delete,
+         name='post_confirm_delete'),
+    path('post/<int:pk>/delete/', blog_views.post_delete, name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from django.contrib import messages
+from .forms import PostForm
 
 # Create your views here.
 
 
 def post_list(request):
-    post_list = Post.objects.all()
-    post_list.reverse()
+    post_list = Post.objects.all().order_by('-date_posted')
     context = {
         'post_list': post_list
     }
@@ -19,3 +20,43 @@ def post_detail(request, pk):
         'post': post
     }
     return render(request, 'blog/post_detail.html', context)
+
+
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    messages.success(request, 'Post deleted successfully')
+    return redirect('post_list')
+
+
+def post_confirm_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_confirm_delete.html', context)
+
+
+def post_edit(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Post edited successfully')
+            return redirect('post_list')  # Redirect to the post list page
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Post added successfully')
+            return redirect('post_list')  # Redirect to the post list page
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_new.html', {'form': form})
