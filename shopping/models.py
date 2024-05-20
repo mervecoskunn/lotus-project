5604b1cfaee0398c9b206da49e5d6e7639947c5c
Merve Coskun <merve_coskun2013@hotmail.com>
2024-05-20 09:56:33 +0300

    free shipping lower bound + banner

--- shopping/models.py
+++ shopping/models.py
@@ -99,7 +99,7 @@ class Cart(models.Model):
         self.subtotal = 0
         for item in self.items.all():
             self.subtotal += item.subtotal
-        self.shipping = 0 if self.subtotal >= 150 else 10
+        self.shipping = 0 if self.subtotal >= 50 else 10
         self.total = self.subtotal + self.shipping
         self.save()
 
