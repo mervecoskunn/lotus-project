5e1cc037044b691608f7cc1f4bb686cf79c69d48
Merve Coskun <merve_coskun2013@hotmail.com>
2024-05-20 09:45:52 +0300

    ProductForm styling

--- shopping/forms.py
+++ shopping/forms.py
@@ -7,11 +7,11 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price', 'category', 'description', 'rating', 'img']
-        # widgets = {
-        #     'name': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'price': forms.NumberInput(attrs={'class': 'form-control'}),
-        #     'category': forms.Select(attrs={'class': 'form-control'}),
-        #     'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-        #     'rating': forms.NumberInput(attrs={'class': 'form-control'}),
-        #     'img': forms.ClearableFileInput(attrs={'class': 'form-control-file'})
-        # }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'style': 'max-width: 300px;'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'min': '0', 'style': 'max-width: 300px;'}),
+            'category': forms.Select(attrs={'class': 'form-control', 'style': 'max-width: 300px;'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'style': 'max-width: 300px;'}),
+            'rating': forms.NumberInput(attrs={'class': 'form-control', 'min': '0', 'style': 'max-width: 300px;'}),
+            'img': forms.ClearableFileInput(attrs={'class': 'form-control-file', 'style': 'max-width: 300px;'})
+        }
