549f7f81391afdc9a47389c864fa919cc1c46f27
Merve Coskun <merve_coskun2013@hotmail.com>
2024-05-20 21:44:14 +0300

    email on new post

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from django.shortcuts import render, redirect
-from .models import Post
+from .models import Post, Newsletter
 from django.contrib import messages
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
 
 
 def post_list(request):
@@ -57,13 +60,41 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+def send_email_to_subscribers(request, post):
+    """
+    Sends an email to all subscribers
+    """
+    recipient_list = [
+        subscriber.email for subscriber in Newsletter.objects.all()]
+
+    html_content = render_to_string(
+        template_name="blog/post_email.html",
+        context={
+            'domain': get_current_site(request).domain,
+            "protocol": "https" if request.is_secure() else "http",
+            "post_id": post.id,
+        }
+    )
+    plain_message = strip_tags(html_content)
+
+    send_mail(
+        subject='New Post Alert',
+        message=plain_message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=recipient_list,
+        html_message=html_content,
+        fail_silently=True
+    )
+
+
 @login_required
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            post = form.save()
             messages.success(request, 'Post added successfully')
+            send_email_to_subscribers(request, post)
             return redirect('post_list')  # Redirect to the post list page
     else:
         form = PostForm()
