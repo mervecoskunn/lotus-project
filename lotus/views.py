41de898c4441d1c944307ea28ea7c51378c1e210
Merve Coskun <merve_coskun2013@hotmail.com>
2024-05-22 07:44:42 +0300

    indentation fix

--- lotus/views.py
+++ lotus/views.py
@@ -104,31 +104,30 @@ def contact(request):
         name = request.POST.get('name')
         email = request.POST.get('email').strip()
         message = request.POST.get('message')
-           email, " message: ", message)
-                # Get contact email
-                html_content= render_to_string(
-            template_name = "lotus/contact_email.html",
-            context = {
+        # Get contact email
+        html_content = render_to_string(
+            template_name="lotus/contact_email.html",
+            context={
                 'name': name,
                 'email': email,
                 'message': message,
                 "protocol": "https" if request.is_secure() else "http",
             }
         )
-            plain_message = strip_tags(html_content)
+        plain_message = strip_tags(html_content)
 
-            send_mail_return = send_mail(
-            subject = 'New Contact Message',
-            message = plain_message,
-            from_email = settings.EMAIL_HOST_USER,
-            recipient_list = [settings.EMAIL_HOST_USER],
-            html_message = html_content,
-            fail_silently = True
+        send_mail_return = send_mail(
+            subject='New Contact Message',
+            message=plain_message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.EMAIL_HOST_USER],
+            html_message=html_content,
+            fail_silently=True
         )
 
-            contact = models.Contact.objects.create(
-            name =name, email=email, message=message)
-            contact.save()
+        contact = models.Contact.objects.create(
+            name=name, email=email, message=message)
+        contact.save()
 
-            messages.success(request, "Your message has been sent successfully!")
-            return render(request, 'lotus/contact.html')
+        messages.success(request, "Your message has been sent successfully!")
+        return render(request, 'lotus/contact.html')
